b4294dc6e69d985205e9cda6b9bcab1f32fa6cae
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-08-18 22:21:44 +0300

    I can't seem to be able to get the button to click so far...

---
+++ popup.js
@@ -0,0 +1,9 @@
+console.log("popup is loaded!!")
+
+// document.addEventListener('DOMContentLoaded', function() {
+// 	document.getElementById('playButton').addEventListener('click', function() {
+// 		console.log('Play button clicked!');
+// 		// Add your action here, like sending a message or executing a function
+// 	});
+// });
+
