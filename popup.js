5d36d1e68580caad15493c11958da26705a3bf49
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-10-09 19:18:56 +0300

    cleared out the project to start over with a clean architecture

--- popup.js
+++
@@ -1,9 +0,0 @@
-console.log("popup is loaded!!")
-
-// document.addEventListener('DOMContentLoaded', function() {
-// 	document.getElementById('playButton').addEventListener('click', function() {
-// 		console.log('Play button clicked!');
-// 		// Add your action here, like sending a message or executing a function
-// 	});
-// });
-
