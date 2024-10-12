84eb5db776dd3cf21b9f44b2cc2efc7a3eb85f96
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-10-12 13:28:01 +0300

    added background.js and reading the messages from the popup frontend

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onMessage.addListener((data) => {
+  switch (data.event) {
+    case "onStop":
+      console.log("On stop in background");
+      break;
+    case "onStart":
+      handleOnStart(data.prefs);
+      break;
+    default:
+      break;
+  }
+});
+
+const handleOnStart = (prefs) => {
+  console.log("On start in background");
+  console.log(prefs);
+  chrome.storage.local.set(prefs);
+};
--- popup/popup.js
+++ popup/popup.js
@@ -5,6 +5,18 @@ const friendKeyElement = document.getElementById("friendKey");
 // Button elements
 const linkButtonElement = document.getElementById("linkButton");
 
-linkButtonElement.onclick = function () {
+linkButtonElement.onclick = () => {
+  const prefs = {
+    friendKey: friendKeyElement.value,
+    personalKey: personalKeyElement.value,
+  };
+  chrome.runtime.sendMessage({ event: "onStart", prefs });
   friendKeyElement.value = "TEST-VALUE";
 };
+
+chrome.storage.local.get(["friendKey", "personalKey"], (result) => {
+  const { friendKey, personalKey } = result;
+  console.log("from the local storage");
+  console.log("personal key from the storage:", personalKey);
+  console.log("friend key from the storage:", friendKey);
+});
