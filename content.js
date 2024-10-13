518756d16af4ff6a0256f2d8100f2631536eed26
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-10-13 17:02:05 +0300

    added the detection to only work on netflix; the content script now consistently logs current time details every time the time is changing

--- content.js
+++ content.js
@@ -1,24 +1,25 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+console.log("content script loaded");
 
-sleep(4000).then(() => {
+let lastRun = 0;
+function throttleHandleVideo() {
+  const now = Date.now();
+  if (now - lastRun >= 300) {
+    handleVideo();
+    lastRun = now;
+  }
+}
+function handleVideo() {
   const video = document.querySelector("video");
 
   if (video) {
     console.log("Current time", video.currentTime);
-  } else {
-    console.log("No video element found on this page!");
   }
-  console.log("waiting 2 seconds...");
-  sleep(2000).then(() => {
-    video.pause();
-    console.log("Video Paused!");
-    console.log("Video time at pausing:", video.currentTime);
-    sleep(2000).then(() => {
-      console.log("Taking the video back 10 seconds...");
-      video.currentTime = 10;
-      console.log("Video time after going back", video.currentTime);
-    });
-  });
+}
+
+const observer = new MutationObserver((mutation) => {
+  throttleHandleVideo();
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+handleVideo();
