b78104997527cc54a8b41700c03d333b3ce4a612
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-11-10 22:49:45 +0300

    fixed the manifest.js error, added the server ip directly in the request

--- popup/popup.js
+++ popup/popup.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const password = document.getElementById("signUpPassword").value;
       const username = document.getElementById("signUpUsername").value;
 
-      const response = await fetch(API_URL + "/register", {
+      const response = await fetch("http://127.0.0.1:8080/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, username, password }),
