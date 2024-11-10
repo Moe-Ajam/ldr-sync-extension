1f7aeebd5fd2016c70086df70bca24a4d9b74a3e
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2024-11-10 22:04:00 +0300

    added the login and signup screen; calling apis is still bugged

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,51 @@
+API_URL = "127.0.0.1:8080/api";
+document.addEventListener("DOMContentLoaded", () => {
+  const signInScreen = document.getElementById("signInScreen");
+  const signUpScreen = document.getElementById("signUpScreen");
+  const mainContent = document.getElementById("mainContent");
+
+  showSignIn();
+
+  function showSignIn() {
+    signInScreen.style.display = "block";
+    signUpScreen.style.display = "none";
+    mainContent.style.display = "none";
+  }
+  function showSignUp() {
+    signInScreen.style.display = "none";
+    signUpScreen.style.display = "block";
+    mainContent.style.display = "none";
+  }
+  function showMainContent() {
+    signInScreen.style.display = "none";
+    signUpScreen.style.display = "none";
+    mainContent.style.display = "block";
+  }
+
+  document.getElementById("showSignUp").addEventListener("click", showSignUp);
+  document.getElementById("showSignIn").addEventListener("click", showSignIn);
+
+  // Form submission for Sign Up
+  document
+    .getElementById("signUpForm")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const email = document.getElementById("signUpEmail").value;
+      const password = document.getElementById("signUpPassword").value;
+      const username = document.getElementById("signUpUsername").value;
+
+      const response = await fetch(API_URL + "/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, username, password }),
+      });
+      if (response.ok) {
+        showMainContent();
+      } else {
+        alert("Sign up failed");
+      }
+    });
+});
 // Elements
 const personalKeyElement = document.getElementById("personalKey");
 const friendKeyElement = document.getElementById("friendKey");
